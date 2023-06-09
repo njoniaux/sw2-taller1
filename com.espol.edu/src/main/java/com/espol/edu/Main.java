695b7844e0c71837195e77028dc1b1dee265d26e
njoniaux <70422868+njoniaux@users.noreply.github.com>
2023-06-08 19:01:47 -0500

    Add files via upload

--- com.espol.edu/src/main/java/com/espol/edu/Main.java
+++ com.espol.edu/src/main/java/com/espol/edu/Main.java
@@ -1,19 +1,31 @@
-// Copyright (C) 2020
-// All rights reserved
 package com.espol.edu;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Main class to run the VacationPackage application.
- * @author YourName
+ * The main class to run the vacation package cost calculation.
  */
 public class Main {
-
+	
     /**
-     * The main method to start the application.
-     * @param args Command line arguments (not used).
+     * The main method to start the program.
+     *
+     * @param args the command-line arguments
      */
     public static void main(String[] args) {
         VacationPackage vacationPackage = new VacationPackage();
-        System.out.println(vacationPackage.calculatePackageCost("Paris", 5, 10));
+
+        // Select add-ons
+        Map<String, Boolean> selectedAddOns = new HashMap<String, Boolean>();
+        selectedAddOns.put("All-Inclusive Package", true);
+        selectedAddOns.put("Adventure Activities Package", false);
+        selectedAddOns.put("Spa and Wellness Package", true);
+
+        // Calculate cost
+        int cost = vacationPackage.calculatePackageCost("Paris", 5, 10, selectedAddOns);
+
+        // Print the total cost of the vacation package
+        System.out.println(cost);
     }
 }
--- com.espol.edu/src/main/java/com/espol/edu/VacationPackage.java
+++ com.espol.edu/src/main/java/com/espol/edu/VacationPackage.java
@@ -1,107 +1,100 @@
-// Copyright (C) 2020
-// All rights reserved
-
 package com.espol.edu;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * VacationPackage class represents a vacation package cost estimator.
- * @author YourName
+ * Represents a vacation package with configurable options and costs.
  */
 public class VacationPackage {
 
-    /** Base cost for a vacation package. */
-    private int baseCost = 1000;
+    // Base cost of the vacation package
+    private final int baseCost = 1000;
 
-    /** Group discount threshold for 10% discount. */
-    private int groupDiscountThreshold1 = 4;
+    // Thresholds and discounts for group sizes
+    private final int groupDiscountThreshold1 = 4;
+    private final int groupDiscountThreshold2 = 10;
+    private final double groupDiscount1 = 0.10;
+    private final double groupDiscount2 = 0.20;
 
-    /** Group discount threshold for 20% discount. */
-    private int groupDiscountThreshold2 = 10;
+    // Maximum group size allowed
+    private final int maxGroupSize = 80;
 
-    /** Maximum group size. */
-    private int maxGroupSize = 80;
+    // Duration and cost-related variables
+    private final int penaltyDuration = 7;
+    private final int promotionalDuration = 30;
+    private final int promotionalPassengerCount = 2;
+    private final int penaltyFee = 200;
+    private final int promotionDiscount = 200;
 
-    /** Discount for groups larger than groupDiscountThreshold1. */
-    private double groupDiscount1 = 0.10;
-
-    /** Discount for groups larger than groupDiscountThreshold2. */
-    private double groupDiscount2 = 0.20;
-
-    /** Duration below which penalty applies. */
-    private int penaltyDuration = 7;
-
-    /** Duration above which promotional discount applies. */
-    private int promotionalDuration = 30;
-
-    /** Number of passengers for which promotional discount applies. */
-    private int promotionalPassengerCount = 2;
-
-    /** Penalty fee for short vacations. */
-    private int penaltyFee = 200;
-
-    /** Promotional discount for long vacations and exactly 2 passengers. */
-    private int promotionDiscount = 200;
-
-    /** Additional cost for popular tourist spots. */
-    private Map<String, Integer> touristSpotsCost;
+    // Cost mappings for tourist spots and add-ons
+    private Map<String, Integer> touristSpotsCost; //NOPMD Map cant be final
+    private Map<String, Integer> addOnsCost; //NOPMD Map cant be final
 
     /**
-     * Constructor initializes the touristSpotsCost map.
+     * Constructs a new VacationPackage instance and initializes the cost mappings.
      */
     public VacationPackage() {
         touristSpotsCost = new HashMap<String, Integer>();
         touristSpotsCost.put("Paris", 500);
         touristSpotsCost.put("New York City", 600);
+
+        addOnsCost = new HashMap<String, Integer>();
+        addOnsCost.put("All-Inclusive Package", 200);
+        addOnsCost.put("Adventure Activities Package", 150);
+        addOnsCost.put("Spa and Wellness Package", 100);
     }
 
     /**
-     * Calculates the cost of a vacation package.
-     * @param destination The destination of the vacation.
-     * @param travelers The number of travelers.
-     * @param duration The duration of the vacation in days.
-     * @return The total cost of the vacation package or -1 if the input data is invalid.
+     * Calculates the total cost of the vacation package based on the specified inputs.
+     *
+     * @param destination     the destination of the vacation
+     * @param travelers       the number of travelers
+     * @param duration        the duration of the vacation
+     * @param selectedAddOns  a map indicating which add-ons are selected
+     * @return the total cost of the vacation package, or -1 if the inputs are invalid
      */
-    public int calculatePackageCost(String destination, int travelers, int duration) {
+    public int calculatePackageCost(String destination, int travelers, int duration, Map<String, Boolean> selectedAddOns) {
         if (!validateInputs(destination, travelers, duration)) {
             return -1;
         }
 
         int cost = baseCost;
 
-        // add additional cost if destination is a popular tourist spot
         if (touristSpotsCost.containsKey(destination)) {
             cost += touristSpotsCost.get(destination);
         }
 
-        // add base cost per traveler
         cost *= travelers;
 
-        // apply group discount
         if (travelers > groupDiscountThreshold1 && travelers <= groupDiscountThreshold2) {
             cost -= cost * groupDiscount1;
         } else if (travelers > groupDiscountThreshold2) {
             cost -= cost * groupDiscount2;
         }
 
-        // apply penalty or discount based on duration
         if (duration < penaltyDuration) {
             cost += penaltyFee;
         } else if (duration > promotionalDuration || travelers == promotionalPassengerCount) {
             cost -= promotionDiscount;
         }
 
+        for (Map.Entry<String, Boolean> entry : selectedAddOns.entrySet()) {
+            if (entry.getValue() && addOnsCost.containsKey(entry.getKey())) {
+                cost += addOnsCost.get(entry.getKey()) * travelers;
+            }
+        }
+
         return cost;
     }
 
     /**
-     * Validates the inputs for a vacation package.
-     * @param destination The destination of the vacation.
-     * @param travelers The number of travelers.
-     * @param duration The duration of the vacation in days.
-     * @return true if the inputs are valid, false otherwise.
+     * Validates the inputs for the vacation package.
+     *
+     * @param destination  the destination of the vacation
+     * @param travelers    the number of travelers
+     * @param duration     the duration of the vacation
+     * @return true if the inputs are valid, false otherwise
      */
     private boolean validateInputs(String destination, int travelers, int duration) {
         if (destination == null || destination.isEmpty() || travelers <= 0 || travelers > maxGroupSize || duration <= 0) {
